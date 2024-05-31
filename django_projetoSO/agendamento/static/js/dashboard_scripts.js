ff99b26c70248923bc768756e7a837c5f435915b
Alef <44003380+alefkaian@users.noreply.github.com>
2024-05-31 11:27:40 -0300

    Revert "v0.1"
    This reverts commit 91d7735a5ecfb0e19b25a40112379fe8fbb520fa.

--- django_projetoSO/agendamento/static/js/dashboard_scripts.js
+++
@@ -1,8 +0,0 @@
-function toggleInfo(id) {
-	var info = document.getElementById(id);
-	if (info.style.display === "none") {
-		info.style.display = "table-row";
-	} else {
-		info.style.display = "none";
-	}
-}
